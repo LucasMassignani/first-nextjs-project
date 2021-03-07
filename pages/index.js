898079c5dcf50376625d2aad3e9aeccfd13db352
Lucas Massignani <lucasmassignani@gmail.com>
2021-03-06 22:39:44 -0300

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 function Home() {
   return (
     <div>
       <h1>Página inicial</h1>
+      <Link href="/wilho">
+        <a>Fala Wilho</a>
+      </Link>
     </div>
   )
 }
---
+++ pages/wilho.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Wilho() {
+  return (
+    <div>
+      <h1>Fala Wilho</h1>
+      <Link href="/">
+        <a>Fala Home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Wilho
