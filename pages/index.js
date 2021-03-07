e0b113cde42e56ee133d85137ab01065c8350197
Lucas Massignani <lucasmassignani@gmail.com>
2021-03-06 21:30:04 -0300

    Alterando Home

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import { useState, useCallback } from 'react'
-
 function Home() {
-  const [contador, setContador] = useState(0)
-
-  const adicionarContador = useCallback(()=>{
-    setContador((state)=>{
-      return state+1
-    })
-  },[])
-
   return (
     <div>
-      <h1>Hello world {contador}</h1>
-      <button onClick={adicionarContador}>Adicionar</button>
+      <h1>Hello world</h1>
     </div>
   )
 }
