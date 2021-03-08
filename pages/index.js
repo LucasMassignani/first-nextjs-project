92374882787e0f83e602e0a8490ad3f8be00cc78
Lucas Massignani Coelho da Silva <46912595+LucasMassignani@users.noreply.github.com>
2021-03-08 13:08:22 -0300

    Teste

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function Home() {
   return (
     <div>
-      <h1>Página inicial</h1>
+      <h1>Página inicial TESTE</h1>
       <Link href="/sobre">
         <a>Pagina sobre</a>
       </Link>
